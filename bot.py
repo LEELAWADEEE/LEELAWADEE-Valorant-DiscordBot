8e8224f697c580842e123b2ad96a76849540ea78
LEELAWADEEE <83025307+LEELAWADEEE@users.noreply.github.com>
2022-08-12 19:07:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ class ValorantBot(commands.Bot):
         
         # bot presence
         activity_type = discord.ActivityType.listening
-        await self.change_presence(activity=discord.Activity(type=activity_type, name="(╯•﹏•╰)"))
+        await self.change_presence(activity=discord.Activity(type=activity_type, name="𝒔𝒚𝒍𝒍𝒗𝒆𝒆 𝒔𝒂𝒅𝒊𝒔𝒕𝒊𝒄"))
     
     async def setup_hook(self) -> None:
         if self.session is None:
